ead5dcfa31167422904ea988f3b80d21c9c1bedc
DoryanAlexandro <doryanamt@gmail.com>
2025-03-26 12:59:40 +0100

    Add project files.

---
+++ MvcCoreApiCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiCrudDepartamentos.Models;
+using MvcCoreApiCrudDepartamentos.Services;
+
+namespace MvcCoreApiCrudDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private ServiceDepartamentos service;
+        public DepartamentosController(ServiceDepartamentos service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Departamento> data = await this.service.GetDepartamentosAsync();
+            return View(data);
+        }
+        public async Task<IActionResult> Details
+            (int id)
+        {
+            Departamento data = await this.service.FindDepartamentoAsync(id);
+            return View(data);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create
+            (Departamento departamento)
+        {
+            await this.service.InsertDepartamentoAsync(departamento.IdDepartamento,
+                departamento.Nombre, departamento.Localidad);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Cliente()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MvcCoreApiCrudDepartamentos/wwwroot/js/serviceapidepartamentos.js
@@ -0,0 +1,67 @@
+﻿let url = "https://apicorecruddepartamentosdamt.azurewebsites.net/";
+function getDepartamentosAsync(callBack) {
+    //console.log("Dentro de JS")
+    let request = "api/departamentos";
+    $.ajax({
+        url: url + request,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            //console.log("Aqui en Ajax");
+            //console.log(data);
+            callBack(data);
+        }
+    })
+}
+
+//  PARA GENERAR EL JSON DE DEPARTAMENTO
+function convertDeptJson(id, nombre, localidad) {
+    let dept = new Object();
+    dept.idDepartamento = id;
+    dept.nombre = nombre;
+    dept.localidad = localidad;
+
+    var json = JSON.stringify(dept);
+    return json;
+}
+
+//  EN LA FUNCION PARA QUE SEA DE FORMA ASINCRONA SE DEBE INCLUIR callBack
+function insertDepartamentoAsync(id, nombre, localidad, callBack) {
+    let json = convertDeptJson(id, nombre, localidad);
+    console.log(json)
+    var request = "api/departamentos";
+    $.ajax({
+        url: url + request,
+        type: "POST",
+        data: json,
+        contentType: "application/json",
+        success: function () {
+            callBack();
+        }
+    })
+}
+
+function updateDepartamentoAsync(id, nombre, localidad, callBack) {
+    let json = convertDeptJson(id, nombre, localidad);
+    var request = "api/departamentos";
+    $.ajax({
+        url: url + request,
+        type: "PUT",
+        data: json,
+        contentType: "application/json",
+        success: function () {
+            callBack();
+        }
+    })
+}
+
+function deleteDepartamentoAsync(id, callBack) {
+    var request = "api/departamentos/"+id;
+    $.ajax({
+        url: url + request,
+        type: "DELETE",
+        success: function () {
+            callBack();
+        }
+    })
+}
